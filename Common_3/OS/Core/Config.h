836eb8a67c26e667871ffbb9109c354ac1420f39
zhaoyao <yaoyao_cn@foxmail.com>
2024-09-05 10:12:45 +0800

    enable ENABLE_FS_EMPTY_MOUNT

--- Common_3/OS/Core/Config.h
+++ Common_3/OS/Core/Config.h
@@ -215,7 +215,7 @@ COMPILE_ASSERT(sizeof(ssize_t) == sizeof(int64_t));
 #define ENABLE_MESHOPTIMIZER
 //Uncomment this to enable empty mounts
 //used for absolute paths
-//#define ENABLE_FS_EMPTY_MOUNT
+#define ENABLE_FS_EMPTY_MOUNT
 
 
 #ifdef ENABLE_PROFILER
